1ae047e56507a8b1436b5437f931aa3f93c3ec9a
Samuel Cornet <samuel@noukod.com>
2019-02-04 19:05:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+
+function findMatching(drivers, fullname){
+    return drivers.filter(driver => driver.toLowerCase() === fullname.toLowerCase());
+}
+
+function fuzzyMatch(drivers, chars) {
+ return drivers.filter(function (driver){return driver.slice(0, chars.length) == chars});
+}
+function matchName(drivers, word){
+  return drivers.filter(function (driver){return driver["name"].match(word)});
+}

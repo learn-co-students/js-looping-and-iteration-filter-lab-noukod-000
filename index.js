5bebe167f8d331d977be959324f53dcca1fe1375
Gilbert Torchon <gilbert@noukod.com>
2019-02-07 18:34:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(array,name){
+  return array.filter(function(element){
+    return element.toLowerCase()==name.toLowerCase();
+  })
+}
+function fuzzyMatch(array,letters){
+  return array.filter((element)=>element.startsWith(letters));
+}
+
+function matchName(array,name){
+  return array.filter((element)=>element.name===name);
+}

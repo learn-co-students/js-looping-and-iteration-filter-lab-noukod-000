cd96c2a6dddd008747ff39df139bb1686559cbd9
RickyyCoder <ricardopaul459@gmail.com>
2019-09-03 11:17:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file
+const drivers = [
+	{
+	  name: 'Bobby',
+	  hometown: 'Pittsburgh' },
+	{
+	  name: 'Sammy',
+	  hometown: 'New York' } ,
+	{
+	  name: 'Sally',
+	  hometown: 'Cleveland' },
+	{
+	  name: 'Annette',
+	  hometown: 'Los Angeles' },
+	{
+	  name: 'Bobby',
+	  hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(drivers, name){
+	const driver = drivers.filter( (driver) => driver.toLowerCase() == name.toLowerCase() )
+	return driver
+}
+
+
+function fuzzyMatch(drivers, partialName){
+	return drivers.filter( (driverName) => driverName.slice(0, partialName.length) === partialName )
+}
+
+function matchName(drivers, name){
+	return drivers.filter( (driver) => driver.name === name );
+}
